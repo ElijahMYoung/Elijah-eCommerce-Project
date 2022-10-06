3d440204a6a6b969024669f9b1171f52678fe59b
Elijah Young <elijah.marlony@gmail.com>
2022-10-06 01:19:12 -0400

    Update app.js

--- app.js
+++ app.js
@@ -30,3 +30,29 @@ btns.forEach((btn, i) => {
     currentSlide = i;
   });
 });
+
+var repeat = function (activeClass) {
+  let active = document.getElementsByClassName("active");
+  let i = 1;
+
+  var repeater = () => {
+    setTimeout(function () {
+      [...active].forEach((activeSlide) => {
+        activeSlide.classList.remove("active"); //prevents active from stacking if you click
+      });
+      slides[i].classList.add("active");
+      btns[i].classList.add("active");
+      i++;
+
+      if (slides.length == i) {
+        i = 0;
+      }
+      if (i >= slides.length) {
+        return;
+      }
+      repeater();
+    }, 10000);
+  };
+  repeater();
+};
+repeat();
